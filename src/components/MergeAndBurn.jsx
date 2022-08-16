f109e74e15abfdea48c9481b1028bfb622a0c182
earlybail <earlybail.contact@gmail.com>
2022-08-16 13:03:14 -0700

    [wip] traits + dynamic image map

--- src/components/MergeAndBurn.jsx
+++ src/components/MergeAndBurn.jsx
@@ -83,7 +83,7 @@ const sx = {
       flexDirection: 'column',
       background: 'none',
       padding: 0,
-      border: '6px double',
+      border: '4px solid',
       outline: 'none',
       borderColor: 'transparent',
     },
@@ -96,76 +96,103 @@ const sx = {
 
 const layers = [
   {
-    trait_type: 'Upper Torso Traits',
-    filePrefix: 'UT',
-  },
-  {
-    trait_type: 'Upper Torso Parts',
-    filePrefix: 'UT',
-  },
-  {
-    trait_type: 'MUZZLE',
-    filePrefix: 'Muzzle',
-  },
-  {
-    trait_type: 'Lower Torso Traits',
-    filePrefix: 'LT',
-  },
-  {
-    trait_type: 'LOWER TORSO PARTS',
-    filePrefix: 'LT',
-  },
-  {
-    trait_type: 'HEAD',
-    filePrefix: '',
+    trait_type: 'Crown',
+    filePrefix: 'CROWN',
+    coords: '265,91,317,172,388,225,474,242,598,226,677,190,752,99,784,0,588,0,367,-1,238,-1',
   },
   {
     trait_type: 'EYES',
     filePrefix: 'Eyes',
+    coords: '465,303,404,306,362,326,340,371,355,419,396,441,432,426,461,406,492,407,536,432,582,444,613,426,632,388,630,354,588,321,519,304',
   },
   {
-    trait_type: 'Crown',
-    filePrefix: 'CROWN',
+    trait_type: 'MUZZLE',
+    filePrefix: 'Muzzle',
+    coords: '457,393,421,416,366,432,352,482,379,524,430,531,480,532,556,533,604,516,597,467,544,434,502,395',
   },
   {
-    trait_type: 'BACKGROUNDS',
-    filePrefix: 'BG',
+    trait_type: 'HEAD',
+    filePrefix: '',
+    coords: '282,169,271,270,291,393,330,511,395,540,498,548,618,534,680,482,705,421,742,305,715,203,600,175,510,151,391,159',
   },
   {
-    trait_type: 'ARMS TOP PARTS',
-    filePrefix: 'AT',
+    trait_type: 'Upper Torso Traits',
+    filePrefix: 'UT',
+    coords: '436,580,389,592,360,646,351,679,340,745,356,785,388,804,437,786,459,745,472,688,487,636,481,591',
   },
   {
-    trait_type: 'ARMS R3 TRAITS',
-    filePrefix: 'ARM R3',
+    trait_type: 'Upper Torso Parts',
+    filePrefix: 'UT',
+    coords: '472,554,405,548,373,556,333,614,313,693,311,767,326,827,366,861,425,856,487,832,556,814,620,827,624,721,610,661,598,590,617,559,579,545',
   },
   {
-    trait_type: 'ARMS R2 TRAITS',
-    filePrefix: 'ARM R2',
+    trait_type: 'Lower Torso Traits',
+    filePrefix: 'LT',
+    coords: '440,887,390,883,343,897,323,935,340,963,390,976,422,974,465,965,491,953,493,922,473,897',
   },
   {
-    trait_type: 'ARMS MIDDLE PARTS',
-    filePrefix: 'AM',
-  },
-  {
-    trait_type: 'ARMS LOWER PARTS',
-    filePrefix: 'AL',
-  },
-  {
-    trait_type: 'ARMS L3 TRAITS',
-    filePrefix: 'ARM L3',
-  },
-  {
-    trait_type: 'ARMS L2 TRAITS',
-    filePrefix: 'ARM L2',
+    trait_type: 'LOWER TORSO PARTS',
+    filePrefix: 'LT',
+    coords: '300,999,301,941,309,886,335,840,362,860,419,860,468,839,539,821,614,828,613,857,583,912,599,971,652,982,662,999',
   },
   {
     trait_type: 'ARM R1 TRAITS',
     filePrefix: 'ARM R1',
+    coords: '209,237,251,305,259,384,231,472,162,508,78,494,26,442,9,339,27,247,85,204,144,198',
+  },
+  {
+    trait_type: 'ARMS R2 TRAITS',
+    filePrefix: 'ARM R2',
+    coords: '159,635,142,591,89,550,35,563,9,625,7,690,35,737,88,754,136,734,150,695',
+  },
+  {
+    trait_type: 'ARMS R3 TRAITS',
+    filePrefix: 'ARM R3',
+    coords: '186,865,167,814,128,773,61,767,20,794,6,866,11,934,34,987,98,992,161,986,182,932',
   },
   {
     trait_type: 'ARM L1 TRAITS',
     filePrefix: 'ARM L1',
+    coords: '786,470,785,406,787,315,805,230,835,121,872,81,924,88,961,137,988,218,997,372,998,453,987,520,930,559,869,546',
+  },
+  {
+    trait_type: 'ARMS L2 TRAITS',
+    filePrefix: 'ARM L2',
+    coords: '815,700,813,657,839,614,888,594,930,598,966,618,975,684,967,727,926,778,871,785,826,760',
+  },
+  {
+    trait_type: 'ARMS L3 TRAITS',
+    filePrefix: 'ARM L3',
+    coords: '809,826,850,792,905,790,962,809,988,851,989,923,970,970,924,988,863,986,811,946,800,886',
+  },
+  {
+    trait_type: 'ARMS TOP PARTS',
+    filePrefix: 'AT',
+    coords: [
+      '377,546,310,525,219,463,197,476,165,501,156,520,190,558,217,602,276,642,316,667',
+      '636,546,608,574,602,614,615,663,660,678,723,657,777,617,847,573,862,534,831,505,797,482,747,499,690,532',
+    ],
+  },
+  {
+    trait_type: 'ARMS MIDDLE PARTS',
+    filePrefix: 'AM',
+    coords: [
+      '310,699,263,684,181,650,144,641,135,683,149,733,179,760,242,798,294,819,327,831,309,771',
+      '642,696,617,727,618,783,627,816,660,826,726,808,787,783,827,753,842,707,828,673,795,669,738,688,686,696',
+    ],
+  },
+  {
+    trait_type: 'ARMS LOWER PARTS',
+    filePrefix: 'AL',
+    coords: [
+      '326,851,262,865,205,859,151,840,118,868,105,892,99,917,112,944,152,961,210,979,249,988,287,989,302,955,299,907',
+      '650,844,614,852,594,906,597,957,616,973,680,980,738,972,807,941,844,907,847,873,825,838,779,855,709,855',
+    ],
+  },
+  {
+    trait_type: 'BACKGROUNDS',
+    filePrefix: 'BG',
+    coords: '0,0,1000,1000',
   },
 ]
 
@@ -182,8 +209,8 @@ const MergeAndBurn = () => {
     if (address) {
       getTokens()
       getBurnCount()
-      setPrimaryToken(null)
-      setSecondaryToken(null)
+      // setPrimaryToken(null)
+      // setSecondaryToken(null)
     }
   }, [address])
 
@@ -277,7 +304,7 @@ const MergeAndBurn = () => {
         })
       })
 
-      setActiveAttribute((activeAttribute) => activeAttribute?.value === primaryAttr.value ? primaryAttr : secondaryAttr)
+      setActiveAttribute(activeAttribute?.value === primaryAttr.value ? secondaryAttr : primaryAttr)
     }
   }
 
@@ -319,12 +346,22 @@ const MergeAndBurn = () => {
 
       {address && (
         <>
-          <Grid container spacing={5} sx={{ my: 3 }}>
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              marginBottom: 48,
+            }}
+          >
             {tokens.map((token) => {
               console.log(token)
               return (
                 <div
                   key={token.id}
+                  style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}
                 >
                   <Button
                     onClick={() => {
@@ -333,9 +370,16 @@ const MergeAndBurn = () => {
                     style={{
                       ...sx.token,
                       ...(token.isSelected ? sx.tokenSelected : null),
+                      marginBottom: 12,
+                      padding: 4,
                     }}
                   >
-                    <Typography>
+                    <Typography
+                      style={{
+                        lineHeight: '2',
+                        fontWeight: 'bold',
+                      }}
+                    >
                       {token.name}
                     </Typography>
 
@@ -346,12 +390,18 @@ const MergeAndBurn = () => {
                     />
                   </Button>
 
-                  {token.isSelected && !token.isPrimary && (
+                  {token.isSelected && (tokens.filter(t => t.isSelected)).length === 2 && (
                     <Button
                       variant="contained"
                       onClick={() => {
                         setPrimaryToken(token)
-                        setSecondaryToken(tokens[1])
+                        // setSecondaryToken(tokens[1])
+                      }}
+                      style={{
+                        // display: 'block',
+                        backgroundColor: token.isPrimary ? colors.highlight : 'white',
+                        color: token.isPrimary ? 'white' : colors.highlight,
+                        fontWeight: 'bold',
                       }}
                     >
                       Make Primary
@@ -360,11 +410,11 @@ const MergeAndBurn = () => {
                 </div>
               )
             })}
-          </Grid>
+          </div>
 
           {newToken && (
             <>
-              <Box style={{position: 'relative', width: 1000, height: 1000 }}>
+              <Box style={{position: 'relative', width: 1000, height: 1000, margin: 'auto', marginBottom: 24, }}>
                 <img
                   src={newToken.image}
                   useMap="#image-map"
@@ -395,140 +445,68 @@ const MergeAndBurn = () => {
                   )
                 })}
 
-                <map name="image-map">
-                  <area
-                    onClick={swapAttribute('Crown')}
-                    title="Crown"
-                    coords="265,91,317,172,388,225,474,242,598,226,677,190,752,99,784,0,588,0,367,-1,238,-1"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('EYES')}
-                    title="EYES"
-                    coords="465,303,404,306,362,326,340,371,355,419,396,441,432,426,461,406,492,407,536,432,582,444,613,426,632,388,630,354,588,321,519,304"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('MUZZLE')}
-                    title="MUZZLE"
-                    coords="457,393,421,416,366,432,352,482,379,524,430,531,480,532,556,533,604,516,597,467,544,434,502,395"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('HEAD')}
-                    title="HEAD"
-                    coords="282,169,271,270,291,393,330,511,395,540,498,548,618,534,680,482,705,421,742,305,715,203,600,175,510,151,391,159"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('Upper Torso Traits')}
-                    title="Upper Torso Traits"
-                    coords="436,580,389,592,360,646,351,679,340,745,356,785,388,804,437,786,459,745,472,688,487,636,481,591"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('Upper Torso Parts')}
-                    title="Upper Torso Parts"
-                    coords="472,554,405,548,373,556,333,614,313,693,311,767,326,827,366,861,425,856,487,832,556,814,620,827,624,721,610,661,598,590,617,559,579,545"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('Lower Torso Traits')}
-                    title="Lower Torso Traits"
-                    coords="440,887,390,883,343,897,323,935,340,963,390,976,422,974,465,965,491,953,493,922,473,897"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('LOWER TORSO PARTS')}
-                    title="LOWER TORSO PARTS"
-                    coords="300,999,301,941,309,886,335,840,362,860,419,860,468,839,539,821,614,828,613,857,583,912,599,971,652,982,662,999"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('ARM R1 TRAITS')}
-                    title="ARM R1 TRAITS"
-                    coords="209,237,251,305,259,384,231,472,162,508,78,494,26,442,9,339,27,247,85,204,144,198"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('ARMS R2 TRAITS')}
-                    title="ARMS R2 TRAITS"
-                    coords="159,635,142,591,89,550,35,563,9,625,7,690,35,737,88,754,136,734,150,695"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('ARMS R3 TRAITS')}
-                    title="ARMS R3 TRAITS"
-                    coords="186,865,167,814,128,773,61,767,20,794,6,866,11,934,34,987,98,992,161,986,182,932"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('ARM L1 TRAITS')}
-                    title="ARM L1 TRAITS"
-                    coords="786,470,785,406,787,315,805,230,835,121,872,81,924,88,961,137,988,218,997,372,998,453,987,520,930,559,869,546"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('ARMS L2 TRAITS')}
-                    title="ARMS L2 TRAITS"
-                    coords="815,700,813,657,839,614,888,594,930,598,966,618,975,684,967,727,926,778,871,785,826,760"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('ARMS L3 TRAITS')}
-                    title="ARMS L3 TRAITS"
-                    coords="809,826,850,792,905,790,962,809,988,851,989,923,970,970,924,988,863,986,811,946,800,886"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('ARMS TOP PARTS')}
-                    title="ARMS TOP PARTS"
-                    coords="377,546,310,525,219,463,197,476,165,501,156,520,190,558,217,602,276,642,316,667"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('ARMS TOP PARTS')}
-                    title="ARMS TOP PARTS"
-                    coords="636,546,608,574,602,614,615,663,660,678,723,657,777,617,847,573,862,534,831,505,797,482,747,499,690,532"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('ARMS MIDDLE PARTS')}
-                    title="ARMS MIDDLE PARTS"
-                    coords="310,699,263,684,181,650,144,641,135,683,149,733,179,760,242,798,294,819,327,831,309,771"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('ARMS MIDDLE PARTS')}
-                    title="ARMS MIDDLE PARTS"
-                    coords="642,696,617,727,618,783,627,816,660,826,726,808,787,783,827,753,842,707,828,673,795,669,738,688,686,696"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('ARMS LOWER PARTS')}
-                    title="ARMS LOWER PARTS"
-                    coords="326,851,262,865,205,859,151,840,118,868,105,892,99,917,112,944,152,961,210,979,249,988,287,989,302,955,299,907"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('ARMS LOWER PARTS')}
-                    title="ARMS LOWER PARTS"
-                    coords="650,844,614,852,594,906,597,957,616,973,680,980,738,972,807,941,844,907,847,873,825,838,779,855,709,855"
-                    shape="poly"
-                  />
-                  <area
-                    onClick={swapAttribute('BACKGROUNDS')}
-                    title="BACKGROUNDS"
-                    coords="1,-1,999,999"
-                    shape="rect"
-                  />
+                <map
+                  name="image-map"
+                >
+                  {layers.map((layer) => {
+                    const coords = Array.isArray(layer.coords) ? layer.coords : [layer.coords]
+                    const attr = newToken?.attributes?.find(attr => attr.trait_type === layer.trait_type)
+
+                    return (
+                      <>
+                        {coords.map((coord) => (
+                          <area
+                            onClick={swapAttribute(layer.trait_type)}
+                            title={`${attr?.trait_type}: ${attr?.value}`}
+                            coords={coord}
+                            shape={coord.split(',').length === 4 ? 'rect' : 'poly'}
+                          />
+                        ))}
+                      </>
+                    )
+                  })}
                 </map>
               </Box>
 
-              {activeAttribute && (
-                <Typography>
-                  {activeAttribute.trait_type}: {activeAttribute.value}
-                </Typography>
+              {newToken && (
+                <div
+                  style={{
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    width: 1016,
+                    margin: 'auto',
+                  }}
+                >
+                  {layers.map((layer) => {
+                    const attr = newToken?.attributes?.find(attr => attr.trait_type === layer.trait_type)
+
+                    return (
+                      <div
+                        onClick={swapAttribute(attr.trait_type)}
+                        style={{
+                          cursor: 'pointer',
+                          margin: 8,
+                          width: 187,
+                          borderRadius: 8,
+                          padding: 8,
+                          textAlign: 'center',
+                          display: 'flex',
+                          flexDirection: 'column',
+                          alignItems: 'center',
+                          justifyContent: 'center',
+                          border: `1px solid ${colors.primary}`,
+                          background: attr?.trait_type === activeAttribute?.trait_type
+                            ? colors.highlight
+                            : 'rgba(21, 178, 229, 0.06)',
+                        }}
+                      >
+                        <span style={{ fontSize: '0.8em', color: colors.primary, fontWeight: '600', }}>{attr.trait_type.toUpperCase()}</span>
+                        <span style={{ color: 'white', fontWeight: 'bold', display: 'block', minHeight: '3em' }}>{attr.value}</span>
+                        <span style={{ fontSize: '0.8em', color: 'rgb(138, 147, 155)', }}>X% have this trait</span>
+                      </div>
+                    )
+                  })}
+                </div>
               )}
             </>
           )}
